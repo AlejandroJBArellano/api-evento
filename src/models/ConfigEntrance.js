53f30410c9f85e8981c61a14e10cb95bd2ec5fc2
Alejandro Jesús Arellano <jesusarellano334@gmail.com>
2022-10-03 16:31:15 -0500

    feat: terminal configurable on configtags
    Signed-off-by: Alejandro Jesús Arellano <jesusarellano334@gmail.com>

--- src/models/ConfigEntrance.js
+++ src/models/ConfigEntrance.js
@@ -1,9 +1,24 @@
 const { Schema, model } = require("mongoose");
 
+const BadgeSchema = new Schema({
+    badge: String,
+    message: String,
+    img: {
+        type: String,
+        required: false
+    },
+    fontColor: String,
+})
+
 const ConfigSchema = new Schema({
     topic: String,
     event: String,
-    imgUrl: String
+    imgUrl: String,
+    allowedBadges: [BadgeSchema],
+    deniedBadges: [BadgeSchema],
+    terminalTitle: String,
+    shortDescription: String,
+    fontColor: String
 });
 
 module.exports = model('ConfigEntrance', ConfigSchema);
